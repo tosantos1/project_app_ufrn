b46221fec773ae25949eeebe757f4ee0c7188941
tosantos1 <tiagolivesan@outlook.com>
2021-03-03 20:26:10 -0300

    App UFRN

---
+++ js/app.js
@@ -0,0 +1,15 @@
+// MENU MOBILE
+
+const showMenu = (toggleId, navId) => {
+    const toggle = document.getElementById(toggleId);
+    const nav = document.getElementById(navId);
+
+    if(toggle && nav){
+        toggle.addEventListener('click', () => {
+            nav.classList.toggle('active-menu-mobile');
+            toggle.classList.toggle('active-bx');
+        })
+    }
+}
+
+showMenu('bx', 'menu-mobile')
